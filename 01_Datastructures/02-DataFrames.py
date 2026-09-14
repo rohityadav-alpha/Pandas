5502333df8a6086417c4b69af181dfd2d26062cb
rohityadav-next <yadavratan524@gmail.com>
2026-09-14 20:44:53 +0530

    DataFrame

--- 01_Datastructures/02-DataFrames.py
+++ 01_Datastructures/02-DataFrames.py
@@ -11,3 +11,9 @@ print(b1)
 print()
 print(pd.DataFrame(b,index=[1,2,3,4],columns=["name"])) #this returns only single column
 print(b1["name"][3]) #this returns single value from the column name and index 3 
+
+
+
+dict1={"voval":pd.Series(["a","e","i","o","u"]),"consonent":pd.Series(["b","c","d","f","g"])}
+dict2=pd.DataFrame(dict1,index=[0,1,2,3,4,5,6])
+print(dict2)
