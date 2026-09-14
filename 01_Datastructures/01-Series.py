c40ed4add0edaba4f631a6e633414adcd64a5f4e
rohityadav-next <yadavratan524@gmail.com>
2026-09-14 20:43:11 +0530

    Series

--- 01_Datastructures/01-Series.py
+++ 01_Datastructures/01-Series.py
@@ -46,3 +46,16 @@ print()
 s={2,5,4,3}
 s1=pd.Series(sorted(s))
 print(s1)
+
+
+# Pandas allow nan values
+# this works
+import pandas as pd 
+b1=pd.Series(10,index=[1,2,3,4,5,6])
+b2=pd.Series(10,index=[1,2,3,4])
+print(b1+b2)
+# but this not
+a=[2,5,6,7]
+a1=pd.Series(a,index=[1,2,3,4])
+a2=pd.Series(a,index=[1,2])
+print(a1+a2)
