16a210ac4f1333a36f0faefcab6a53b53bfac913
rohityadav-next <yadavratan524@gmail.com>
2026-09-18 16:08:35 +0530

    Data Manipulation-insert,delete

--- 01_Datastructures/04-Datamanipulation.py
+++ 01_Datastructures/04-Datamanipulation.py
@@ -5,3 +5,10 @@ table={"A":[2,4,3,7,3],"B":[7,9,5,1,6]}
 var=pd.DataFrame(table)
 print(var)
 
+print()
+
+var.insert(2,"C",var["A"]) #(position of the column , name of the column , values to insert into tha column )
+print(var)
+
+print()
+
