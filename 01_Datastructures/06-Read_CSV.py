af7dbe3984445e2117a037b0a27775a3e1a8c64f
rohityadav-next <yadavratan524@gmail.com>
2026-09-19 22:52:46 +0530

    Practice Questions

--- 01_Datastructures/06-Read_CSV.py
+++ 01_Datastructures/06-Read_CSV.py
@@ -15,3 +15,11 @@ print(file1)
 # if you want some specific columns - using usecols parameter
 file1=pd.read_csv("D:\\LEARN_PYTHON\\Day-21_Pandas_lib\\basic-data.csv",usecols=[1,3])
 print(file1)
+
+# if you want to skip specific rows - using skiprows parameter
+file1=pd.read_csv("D:\\LEARN_PYTHON\\Day-21_Pandas_lib\\basic-data.csv",skiprows=[3,5])
+print(file1)
+
+# if you want to set a specific column as index - using index_col parameter
+file1=pd.read_csv("D:\\LEARN_PYTHON\\Day-21_Pandas_lib\\basic-data.csv",index_col="ID")
+print(file1)
