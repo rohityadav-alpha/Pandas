baf0890675505115c49cc4457cb1d028e62f83b8
rohityadav-next <yadavratan524@gmail.com>
2026-09-19 22:54:56 +0530

    Practice Questions

--- 01_Datastructures/06-Read_CSV.py
+++ 01_Datastructures/06-Read_CSV.py
@@ -23,3 +23,11 @@ print(file1)
 # if you want to set a specific column as index - using index_col parameter
 file1=pd.read_csv("D:\\LEARN_PYTHON\\Day-21_Pandas_lib\\basic-data.csv",index_col="ID")
 print(file1)
+
+# if you want to set a specific row as header - using header parameter  and it can none also
+file1=pd.read_csv("D:\\LEARN_PYTHON\\Day-21_Pandas_lib\\basic-data.csv",header=2)
+print(file1)
+
+# if you want to set a specific name of a column or new column - using names parameter
+file1=pd.read_csv("D:\\LEARN_PYTHON\\Day-21_Pandas_lib\\basic-data.csv",names=["ID1","Name1","Age1","City1","email1"])
+print(file1)
