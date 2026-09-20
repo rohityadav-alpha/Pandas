6d7de445e0380d68f5e5886ac49370d43368a898
rohityadav-next <yadavratan524@gmail.com>
2026-09-20 19:36:56 +0530

    Pandas Function

---
+++ 03_Pandas_Function/02-DescribeFun.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+D1=pd.read_csv("D:\\LEARN_PYTHON\\Day-21_Pandas_lib\\03_Pandas_Function\\customers-100.csv")
+print(D1)
+print(D1.describe()) # describe() function is use to get count, mean, std, min, 25%, 50%, 75%, max
